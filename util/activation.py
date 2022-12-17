0c7c5c66e9052c4e4e6701291b3c102d69b5ec9a
asd08573064 <xdmanwww@gmail.com>
2022-12-17 16:28:11 +0800

    create util

---
+++ util/activation.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def Sigmoid(z):
+        return np.clip(1 / (1.0 + np.exp(-z)), 1e-8, 1 - (1e-7))
+
+def Softmax(z):
+    z = z - np.max(z)
+    return np.exp(z)/np.sum(np.exp(z))
+
+def Relu(z):
+    return np.maximum(z, 0)
+
+def cross_entropy(Y, Y_prediction):
+    return -np.sum(np.multiply(Y, np.log(Y_prediction + 1e-07)))
